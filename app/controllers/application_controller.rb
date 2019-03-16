8240e20d0a8e7b15871ca096bd2dafe270a05ecf
N-Ryo <ryo.joy.joy@gmail.com>
2019-03-16 16:05:35 +0900

    Modify sign-up view by devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,49 +1,11 @@
 class ApplicationController < ActionController::Base
-  before_action :set_book, only: [:show, :edit, :update, :destroy]
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
-  def index
-    @books = Book.all
-  end
+  protected
 
-  def new
-    @book = Book.new
-  end
-
-  def create
-    @book = Book.new(book_params)
-    if @book.save
-      redirect_to @book, notice: "書籍を登録しました。"
-    else
-      render :new
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:nick_name, :avatar])
+      devise_parameter_sanitizer.permit(:account_update, keys: [:nick_name, :avatar])
     end
-  end
-
-  def show
-  end
-
-  def edit
-  end
-
-  def update
-    if @book.update(book_params)
-      redirect_to @book, notice: "書籍を更新しました。"
-    else
-      render :edit
-    end
-  end
-
-  def destroy
-    @book.destroy
-    redirect_to books_path, notice: "書籍を削除しました。"
-  end
-
-  private
-
-    def book_params
-      params.require(:book).permit(:title, :price, :publish_date, :description, :new_image, :category_id)
-    end
-
-    def set_book
-      @book = Book.find(params[:id])
-    end
+    
 end
