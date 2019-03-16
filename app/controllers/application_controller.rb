b784370b1da9d4c538ca214ec322892da4efa6eb
N-Ryo <ryo.joy.joy@gmail.com>
2019-03-16 15:01:44 +0900

    Add assosiation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -40,7 +40,7 @@ class ApplicationController < ActionController::Base
   private
 
     def book_params
-      params.require(:book).permit(:title, :price, :publish_date, :description, :new_image)
+      params.require(:book).permit(:title, :price, :publish_date, :description, :new_image, :category_id)
     end
 
     def set_book
